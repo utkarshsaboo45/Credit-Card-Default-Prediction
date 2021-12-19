37089b900af63c308b51a04d6e83031ca4874ad4
Utkarsh Saboo <utkarshsaboo45@gmail.com>
2021-12-18 23:56:59 -0800

    Rename Data File, Add Preprocess.py

---
+++ src/preprocess.py
@@ -0,0 +1,23 @@
+def read_data():
+    # Read data from the csv
+    pass
+
+def clean_data():
+    # Clean data
+    pass
+
+def create_features():
+    # Feature Engineering
+    pass
+
+def split_data():
+    # Split the data
+    pass
+
+def save_data():
+    # Export data to csv file
+    pass
+
+def preprocess_data():
+    # Call all the preprocess functions as required
+    pass
